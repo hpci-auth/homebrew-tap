1d31ce5a48e88964318927343d5dd8248d2c1b5c
Takuya Ishibashi <32759610+soum-takuya@users.noreply.github.com>
2026-03-26 17:27:08 +0000

    hpcissh: add 1.12.0-rc6 bottle.

--- Formula/hpcissh.rb
+++ Formula/hpcissh.rb
@@ -12,6 +12,14 @@ class Hpcissh < Formula
 
   head "https://github.com/hpci-auth/hpcissh-clients.git", branch: "develop"
 
+  bottle do
+    root_url "https://github.com/hpci-auth/homebrew-tap/releases/download/hpcissh-1.12.0-rc6"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "532e5a555a5dc85876001b4479875a53dd6167c818cdf77b7c4737ef655b7062"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "eea0a368604595cdca8866cee23218910fca98b4b4a6e9a306ae9b84a2719815"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f570e64fa990a946a856c9c5573421b5991810a072a90fb120b353f7dca25c0c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "724e08e94d4d25a94adeb51ea08a80e752611f9a2aee01a9b78a98a2ac59f2b9"
+  end
+
   depends_on "jwt-agent"
 
   on_macos do
