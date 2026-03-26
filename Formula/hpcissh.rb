44c2b7cc788e6078677e47c45a1e9bacfeeef965
Takuya Ishibashi <32759610+soum-takuya@users.noreply.github.com>
2026-03-27 02:18:09 +0900

    update for hpcissh-1.12.0-rc6
    Closes #4.
    
    Signed-off-by: Takuya Ishibashi <32759610+soum-takuya@users.noreply.github.com>

--- Formula/hpcissh.rb
+++ Formula/hpcissh.rb
@@ -10,15 +10,7 @@ class Hpcissh < Formula
 
   license "Apache-2.0"
 
-  head "https://github.com/hcpi-auth/hpcissh-clients.git", branch: "develop"
-
-  bottle do
-    root_url "https://github.com/soum-takuya/homebrew-tap-hpcidev/releases/download/hpcissh-1.12.0-rc5"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "dfb482f26de21016e7bc38a8ea15a4323409a715aa705325c90f2c25b2f1549f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ba8c11e94e1c3eb2fb33f03f792b49788de6b070517638cffdfc789b6acb7b49"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "efd1b5c0f0a3be438ac61fd02b260315d08bddf364aaa0c39a9dd15150fa1f7a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5495ab6f820557890843333c8ee77b80756fc09f2b80e21055f1176dc84a03e0"
-  end
+  head "https://github.com/hpci-auth/hpcissh-clients.git", branch: "develop"
 
   depends_on "jwt-agent"
 
